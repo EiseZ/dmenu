5c79406d316a6170520a595b6dff6bd1029e64d1
EiseZ <eise.zimmerman@outlook.com>
2022-12-13 19:50:36 +0100

    Initial commit

---
+++ config.h
@@ -0,0 +1,28 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int instant = 1;
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"SF Mono:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#CDD6F4", "#1E1E2E" },
+	[SchemeSel] = { "#1E1E2E", "#86B4FA" },
+	[SchemeOut] = { "#CDD6F4", "#1E1E2E" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 30;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
